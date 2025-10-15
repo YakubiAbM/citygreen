b16406ba5e541cdfc49e3f57a178c9f078ccd764
Якуби Абдусаттор <abdusattoryaqubi@gmail.com>
2025-10-15 13:29:53 +0500

    Final code ready for Render

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-
+import os
 from typing import List
-BOT_TOKEN = "8353786544:AAFimU0W-xKksDDnULatciYzi9EF0Ri7xVE"
-
 ADMIN_IDS: List[int] = [6318017185] # ID администратора(ов)
 ADMIN_USERNAME = "username_admin"  # Юзернейм администратора для кнопки "Связаться"
 DATABASE_NAME = "citygreen.db"
+BOT_TOKEN = os.getenv("xVE8353786544:AAFimU0W-xKksDDnULatciYzi9EF0Ri7",) # Второй аргумент - резервный для локального запуска
