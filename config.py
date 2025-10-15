6a8a4df47e876244bb3dd77614b419ac8fc7a590
Якуби Абдусаттор <abdusattoryaqubi@gmail.com>
2025-10-15 14:14:38 +0500

    Trigger manual redeploy

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-import os
-from typing import List
-ADMIN_IDS: List[int] = [6318017185] # ID администратора(ов)
-ADMIN_USERNAME = "username_admin"  # Юзернейм администратора для кнопки "Связаться"
-DATABASE_NAME = "citygreen.db"
-BOT_TOKEN = os.getenv("xVE8353786544:AAFimU0W-xKksDDnULatciYzi9EF0Ri7",) # Второй аргумент - резервный для локального запуска
-# config.py
 import os # <-- Убедитесь, что этот импорт есть!
 from typing import List
 
