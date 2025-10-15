bc5b3df495ac169bda534f4ed64e768a796d3632
Якуби Абдусаттор <abdusattoryaqubi@gmail.com>
2025-10-15 13:20:50 +0500

    Final code ready for Render

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-from typing import List
 
-# ⚠️ Вставьте сюда токен вашего бота, полученный от @BotFather
+from typing import List
 BOT_TOKEN = "8353786544:AAFimU0W-xKksDDnULatciYzi9EF0Ri7xVE"
 
-# ⚠️ Вставьте свой реальный ID и юзернейм админа
-# Чтобы узнать свой ID, используйте бота @userinfobot
 ADMIN_IDS: List[int] = [6318017185] # ID администратора(ов)
-ADMIN_USERNAME = "Imabdoo"  # Юзернейм администратора для кнопки "Связаться"
+ADMIN_USERNAME = "username_admin"  # Юзернейм администратора для кнопки "Связаться"
 DATABASE_NAME = "citygreen.db"
